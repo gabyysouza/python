76b44a3f8c04ed0275f361425597111dce48009e
Lorrany Felix <luis.mendes.almeida@escola.pr.gov.br>
2025-05-09 11:20:21 -0300

    exemplo 5 operacoes com strings

---
+++ pythonProject/Capitulo 2/exemplo5operacoesstrings.py
@@ -0,0 +1,10 @@
+# É proibido utilizar operacoes matematicas com strings
+# Ao menos de 2 casos
+
+first = 'Gaby '
+second = 'Souza'
+nomeSobrenome = first + second # + aqui significa contatenaçao
+print(nomeSobrenome)
+
+stringQualquer = "Teste " * 3 # * aqui significa contatenacao o numedo de vezes colocado
+print(stringQualquer)
