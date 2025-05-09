bdc7a16ea0603cd42474dbdd0f865454756491f0
Lorrany Felix <luis.mendes.almeida@escola.pr.gov.br>
2025-05-09 11:42:34 -0300

    exercicio 1

---
+++ pythonProject/Capitulo 2/exercicio1.py
@@ -0,0 +1,15 @@
+num1 = 4
+num2 = 3
+num3 = 3.14
+num4 = 5
+num5 = 3
+
+total = (num1 / num2 ) * num3 * (num4 ** num5)
+print(total)
+
+primeiroTermo = 4 / 3
+pi = 3.14
+raio = 5 **3
+
+total = primeiroTermo + pi + raio # forma mais facil de ler
+print("O valor de uma eesfera de raio 5 é de ", total)
