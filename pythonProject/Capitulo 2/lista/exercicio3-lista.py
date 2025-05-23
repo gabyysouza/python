efd1a0bafe2c121efd70bcc2c08d92e66adaf6cb
Andrey <Andrey.schmitz@escola.pr.gov.br>
2025-05-23 11:20:29 -0300

    exercicio 3 lista

---
+++ pythonProject/Capitulo 2/lista/exercicio3-lista.py
@@ -0,0 +1,13 @@
+raio = int(input("Digite o raio do circulo: "))
+pi = 3.14159
+
+diametro = 2 * raio
+circuferencia = 2 * pi * raio
+area = pi * (raio ** 2)
+
+print("O diametro do circulo é: ", diametro)
+print("A circuferencia do circulo é: ", circuferencia)
+print("A area do circulo é: ", area)
+
+# se nesse caso escolhessemos a opção de ter o resultado de um numero inteiro usariamos 'round'
+
