f0c7e323f55ca792a00aec3f239dc13e99dccfbc
Andrey <Andrey.schmitz@escola.pr.gov.br>
2025-05-23 10:35:30 -0300

    exercicio 1 lista back-end

---
+++ pythonProject/Capitulo 2/lista/exercicio1-lista.py
@@ -0,0 +1,14 @@
+num1 = int(input('Escreva um primeiro numero: '))
+num2 = int(input('Escreva um segundo numero: '))
+
+soma = num1 + num2
+produto = num1 * num2
+diferenca = num1 - num2
+quociente =num1 / num2
+
+print('A soma é; ', soma)
+print('O produto é: ', produto)
+print('A diferença é: ', diferenca)
+print('O quociente é: ', quociente)
+
+
