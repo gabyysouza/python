228585c6940025d8f24d7c7ba42249c89bf80972
endrius <endrius.pontes@escola.pr.gov.br>
2025-10-24 09:59:38 -0300

    subir github

---
+++ pythonProject/exercicio 24/exercicio 1.py
@@ -0,0 +1,12 @@
+import os
+
+for i in range(1, 21):
+    print(i)
+
+input("Pressione Enter para continuar...")
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+for i in range(20, 0, -1):
+    print(i)
+
---
+++ pythonProject/exercicio 24/exercicio 4.py
@@ -0,0 +1,17 @@
+maior = None
+menor = None
+
+for i in range(10):
+    num = int(input(f"Digite o {i + 1}º número positivo: "))
+
+    if num <= 0:
+        print("Por favor, digite apenas números inteiros positivos.")
+        continue
+
+    if maior is None or num > maior:
+        maior = num
+    if menor is None or num < menor:
+        menor = num
+
+print(f"O maior número é: {maior}")
+print(f"O menor número é: {menor}")
